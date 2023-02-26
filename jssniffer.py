8e1d13486594e98e25bab93cd05fdb50ae63f44d
Alessio M <alessio.marziali@gmail.com>
2023-02-26 21:04:00 +0000

    craw_website improvements

--- jssniffer.py
+++ jssniffer.py
@@ -8,6 +8,9 @@ import shutil
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 
+# print list of javascript files the website is using
+outputfoundfiles = True
+
 
 # function to get all JavaScript files on a page
 def get_js_files(url):
@@ -30,29 +33,29 @@ def get_js_files(url):
 
 
 # function to crawl a website
-def crawl_website(url):
+def crawl_website(websiteurl):
     visited = set()
     js_files = set()
-    queue = [url]
+    queue = [websiteurl]
 
     while queue:
         # get next page to visit from queue
-        url = queue.pop(0)
+        websiteurl = queue.pop(0)
 
         # check if page has already been visited
-        if url in visited:
+        if websiteurl in visited:
             continue
 
         try:
-            domain = urlparse(url).netloc
-            page_js_files = get_js_files(url)
+            domain = urlparse(websiteurl).netloc
+            page_js_files = get_js_files(websiteurl)
 
             js_files.update(page_js_files)
-            visited.add(url)
+            visited.add(websiteurl)
             print(".", end="", flush=True)
 
             # find all links on the page
-            response = requests.get(url)
+            response = requests.get(websiteurl)
             soup = BeautifulSoup(response.content, "html.parser")
             links = soup.find_all("a")
 
@@ -61,19 +64,20 @@ def crawl_website(url):
                 href = link.get("href")
                 if href:
                     # resolve relative URLs
-                    href = urljoin(url, href)
+                    href = urljoin(websiteurl, href)
 
                     # check if link is on the same domain and hasn't been visited
                     if urlparse(href).netloc == domain and href not in visited:
                         queue.append(href)
 
         except Exception as e:
-            print(f"\nError crawling website {url}: {str(e)}")
+            print(f"\nError crawling website {websiteurl}: {str(e)}")
 
     # print the list of JavaScript files found
     print(f"\n\nFound {len(js_files)} unique JavaScript files on {domain}:")
-    for js_file in js_files:
-        print(f" - {js_file}")
+    if outputfoundfiles:
+        for js_file in js_files:
+            print(f" - {js_file}")
 
     # create a directory for the JavaScript files
     js_dir = os.path.join(os.getcwd(), "js_files")
@@ -82,8 +86,11 @@ def crawl_website(url):
 
     # download and search for keywords in each JavaScript file
     keywords = ["token"]
+    # create list of files with keywords
+    fileswithkeyword = []
+
     for js_file in js_files:
-        file_url = urljoin(url, js_file)
+        file_url = urljoin(websiteurl, js_file)
         file_name = os.path.basename(js_file)
         file_path = os.path.join(js_dir, file_name)
 
@@ -100,26 +107,34 @@ def crawl_website(url):
             with open(file_path, "rb") as f:
                 try:
                     contents = f.read().decode()
-                    found_keyword = False
                     for keyword in keywords:
                         if keyword.lower() in contents.lower():
-                            print(f"File {file_name} contains keyword '{keyword}'")
-                            found_keyword = True
+                            # print(f"File {file_name} contains keyword '{keyword}'")
+                            fileswithkeyword.append(file_name)
                 except UnicodeDecodeError:
                     os.remove(file_path)
                     print(f"Error decoding {file_path}, skipping and removing file...")
                     continue
+
+    # remove duplicates
+    unique_list = list(set(fileswithkeyword))
+
+    # print unique files
+    for file in unique_list:
+        print(f"File {file} contains one or more keywords")
+
     # delete all javascript files if no keyword found
-    if not found_keyword:
-        if os.path.exists(js_dir):
-            confirm = input(f"Are you sure you want to delete {js_dir}? (y/n): ")
-            if confirm.lower() == "y":
-                shutil.rmtree(js_dir)
-                print(f"Deleted {js_dir}")
-            else:
-                print(f"{js_dir} was not deleted.")
+    if os.path.exists(js_dir):
+        confirm = input(f"Are you sure you want to delete {js_dir}? (y/n): ")
+        if confirm.lower() == "y":
+            shutil.rmtree(js_dir)
+            print(f"Deleted {js_dir}")
+        else:
+            print(f"{js_dir} was not deleted.")
 
 
+# clear console
+os.system('cls' if os.name == 'nt' else 'clear')
 # run the script
 url = input("Enter a URL to crawl: ")
 crawl_website(url)
